253476a48fd2f389693bc3e8711db5620a747f2a
ocean.yu <oceanyu@oceanyudemacbook-pro.local>
2016-04-12 11:28:20 +0800

    Initial Commit

---
+++ RongCloudIMtest_yuhy0412/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RongCloudIMtest_yuhy0412
+//
+//  Created by ocean.yu on 16/4/12.
+//  Copyright © 2016年 ocean.yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
